465bef1f3427d2bc1a392d64c902ed8505a9b76b
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-02-16 16:18:41 -0800

    quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,16 +6,17 @@
 
 /**
  * main - Prints the message which is in the
- * array msg into the standard error 
+ * array msg into the standard error
  *
  *
  * Return: Always 1.
  */
 int main(void)
 {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    const int len = sizeof(msg) - 1;
-    write(STDERR_FILENO, msg, len);
-    return 1;
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = sizeof(msg) - 1;
+
+write(STDERR_FILENO, msg, len);
+return (1);
 }
 
