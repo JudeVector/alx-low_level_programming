34ecad9696230349a2635c0fbd8eb0056809368f
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-02-20 23:22:42 -0800

    Updated putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,38 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * main - Print _putchar with a new line
+ * main - Print _putchar
  * Return: Always 0
  */
 int main(void)
 {
-char c = '_';
-
-write(STDOUT_FILENO, &c, 1);
-c = 'p';
-
-write(STDOUT_FILENO, &c, 1);
-c = 'u';
-
-write(STDOUT_FILENO, &c, 1);
-c = 't';
-
-write(STDOUT_FILENO, &c, 1);
-c = 'c';
-
-write(STDOUT_FILENO, &c, 1);
-c = 'h';
-
-write(STDOUT_FILENO, &c, 1);
-c = 'a';
-
-write(STDOUT_FILENO, &c, 1);
-c = 'r';
-
-write(STDOUT_FILENO, &c, 1);
-c = '\n';
-
-write(STDOUT_FILENO, &c, 1);
-
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
