4f0f062e2ee9edb63110ed0198ac22b1df0c2a94
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-03-10 12:31:54 -0800

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,14 +8,12 @@
 *
 * Return: Always 0 (Success)
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int j;
 
 	for (j = 0; j < argc; j++)
-	{
 		printf("%s\n", argv[j]);
-	}
 
 	return (0);
 }
