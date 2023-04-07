df0311579a81f2414ea028dc4b93e683c2463012
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-04-07 14:37:28 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,21 @@
 #include "main.h"
-
 /**
- * _custom_power - Custom implementation of exponentiation.
+ * _power - Custom implementation of exponentiation.
  * @base: The base value.
  * @pow: The power value.
  *
  * Return: The result of base raised to the power of pow.
  */
-unsigned long int _custom_power(unsigned int base, unsigned int pow)
+unsigned long int _power(unsigned int base, unsigned int pow)
 {
-	unsigned long int num = 1;
+	unsigned long int num;
 	unsigned int i;
 
+	num = 1;
 	for (i = 1; i <= pow; i++)
 		num *= base;
 	return (num);
 }
-
 /**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to print in binary.
@@ -25,17 +24,20 @@ unsigned long int _custom_power(unsigned int base, unsigned int pow)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int dev = _custom_power(2, sizeof(unsigned long int) * 8 - 1);
-	char flag = 0;
+	unsigned long int dev, result;
+	char flag;
+
+	flag = 0;
+	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
 
 	while (dev != 0)
 	{
-		unsigned long int result = n & dev;
-
+		result = n & dev;
 		if (result == dev)
 		{
 			flag = 1;
 			_putchar('1');
+
 		}
 		else if (flag == 1 || dev == 1)
 		{
@@ -44,4 +46,3 @@ void print_binary(unsigned long int n)
 		dev >>= 1;
 	}
 }
-
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-unsigned long int _custom_power(unsigned int base, unsigned int pow)
+unsigned long int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -10,4 +10,4 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int _putchar(char c);
 
-#endif
+#endif 
