403a50fe7f270c96d8731ad2cc61ed7801ca60fd
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-05-05 09:55:07 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness of the system
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int plm = 1;
+	char *ptr = (char *)&plm;
+
+	if (*ptr)
+		return (1);
+	else
+		return (0);
+}
+
+
